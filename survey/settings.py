4c5aabbf5d5c479199b46563c5189e573f0664fa
lolkin <lolkin_321@mail.ru>
2026-02-09 01:22:54 +0300

    Changed settings

--- survey/settings.py
+++ survey/settings.py
@@ -125,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
